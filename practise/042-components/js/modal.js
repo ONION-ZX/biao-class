1015c36b0cec5e6f99207201233eada4f829b6ea
ONION-ZX <zxonion@gmail.com>
2018-04-19 19:11:54 +0800

    finish modal

--- practise/042-components/js/modal.js
+++ practise/042-components/js/modal.js
@@ -1,5 +1,6 @@
 var modal_trigger = document.querySelector('.modal-trigger');
 var modal_content = document.querySelector('.modal');
+var modal_close = document.querySelector('.close')
 var a = document.querySelector('a');
 
 a.addEventListener('click',function(e) {
@@ -21,3 +22,18 @@ function hide() {
 modal_trigger.addEventListener('click',function() {
   visible() ? hide() : show();
 });
+
+// modal_close.addEventListener('click',hide);
+document.addEventListener('click',function(e) {
+  var el = e.target;
+  var menu = el.closest('.modal');
+  if(!menu && el !== modal_close)
+    return;
+  hide();
+});
+
+document.addEventListener('keyup',function(e) {
+  if(e.code !== 'Escape')
+    return;
+  hide();
+})
