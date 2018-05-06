b3a1f8605340794bd11f64fa5a46e6949c0d4f38
ONION-ZX <zxonion@gmail.com>
2018-05-06 11:55:20 +0800

    github pack

--- practise/053-npm-github-search-pack/js/event.js
+++ practise/053-npm-github-search-pack/js/event.js
@@ -1,17 +1,11 @@
 /*事件处理相关*/
 
-var el = require('./element')
-  , search = require('./search')
-  , page = 1
-  , limit = 5
-  , keyword
+var el = require('./element'),
+  search = require('./search'),
+  page = 2,
+  limit = 5,
+  keyword
 ;
-
-function detect_click_top() {
-  el.top.addEventListener('click',function() {
-    window.scrollTo(0,0);
-  });
-}
 
 /*监听搜索表单提交事件*/
 function detect_submit() {
@@ -19,13 +13,15 @@ function detect_submit() {
     e.preventDefault();
     /*获取搜索关键词（获取input元素的值）*/
     keyword = el.input.value;
-
+    if (!keyword || keyword == '') {
+      console.error('输入不可为空!!');
+      return;
+    }
     /*开始搜*/
     search.user(keyword, function (data) {
-      var items = data.items;
+      const items = data.items;
       el.render_user_list(items);
-
-    });
+    },{});
   });
 }
 
@@ -33,7 +29,7 @@ function detect_next_page() {
 
   el.next.addEventListener('click', function () {
     /*准备配置*/
-    var config = {
+    let config = {
       page: page++,
       limit: limit,
     };
@@ -42,7 +38,6 @@ function detect_next_page() {
     search.user(keyword, function (data) {
       el.render_user_list(data.items);
     }, config);
-
   });
 }
 
@@ -50,106 +45,10 @@ function detect_next_page() {
 function add_events() {
   detect_submit();
   detect_next_page();
-  detect_click_top();
 }
 
 module.exports = {
   detect_submit: detect_submit,
   add_events: add_events,
   detect_next_page: detect_next_page,
-  detect_click_top: detect_click_top,
 };
-// var el = require('./element')
-//   , search = require('./search')
-//   , page = 1
-//   , limit = 5
-//   , keyword
-// ;
-//
-// function detect_submit() {
-//   el.form.addEventListener('submit',function(e) {
-//     e.preventDefault();
-//     keyword = input.value;
-//     search.user(keyword,function(data) {
-//       el.render_user_list(data.items);
-//     });
-//   });
-// }
-//
-// function detect_next_page() {
-//   el.next.addEventListener('click',function() {
-//     var config = {
-//       page: page ++,
-//       limit: limit,
-//     };
-//
-//     search.user(keyword,function(data) {
-//       el.render_user_list(data.items);
-//     },config);
-//
-//   });
-// }
-//
-// function add_events() {
-//   detect_submit();
-//   detect_next_page();
-// }
-//
-// module.exports = {
-//   add_events: add_events,
-//   detect_submit: detect_submit,
-//   detect_next_page: detect_next_page,
-// };
-// /*事件处理相关*/
-//
-// var el = require('./element')
-//   , search = require('./search')
-//   , page = 1
-//   , limit = 5
-//   , keyword
-// ;
-//
-// /*监听搜索表单提交事件*/
-// function detect_submit() {
-//   el.form.addEventListener('submit', function (e) {
-//     e.preventDefault();
-//     /*获取搜索关键词（获取input元素的值）*/
-//     keyword = el.input.value;
-//
-//     /*开始搜*/
-//     search.user(keyword, function (data) {
-//       var items = data.items;
-//       el.render_user_list(items);
-//
-//     });
-//   });
-// }
-//
-// function detect_next_page() {
-//
-//   el.next.addEventListener('click', function () {
-//     /*准备配置*/
-//     var config = {
-//       page: page++,
-//       limit: limit,
-//     };
-//
-//     /*开始搜索*/
-//     search.user(keyword, function (data) {
-//       el.render_user_list(data.items);
-//     }, config);
-//
-//   });
-// }
-//
-// /*批量添加所有初始事件*/
-// function add_events() {
-//   detect_submit();
-//   detect_next_page();
-// }
-//
-// module.exports = {
-//   detect_submit: detect_submit,
-//   add_events: add_events,
-//   detect_next_page: detect_next_page,
-// };
