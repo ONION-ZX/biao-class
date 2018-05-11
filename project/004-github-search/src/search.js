20924ebd6b594cb0a36e7cf1ff75707e33ff7f66
ONION-ZX <zxonion@gmail.com>
2018-05-11 09:28:57 +0800

    search reconstruction

--- project/004-github-search/src/search.js
+++ project/004-github-search/src/search.js
@@ -1,6 +1,7 @@
 var ele = require('./element')
   , share = require('./share')
-  , keyword = share.get_keyword();
+  , keyword = share.get_keyword()
+  , limit = share.get_limit()
   , no_more
   , res
 ;
@@ -65,6 +66,86 @@ function reset_user_list() {
 
 function get_page_amount() {
   page_amount = Math.ceil(share.get_amount() / share.get_limit());
+}
+
+function go_to_page(page) {
+  var reach_page_limit = page * limit > MAX_LIMIT;
+
+  if(reach_page_limit) {
+    current_page = share.set_current_page(Math.floor(MAX_LIMIT / limit));
+  } else {
+    current_page = share.set_current_page(page);
+  }
+
+  search();
+}
+
+function show_pagination() {
+  ele.pagination_container.hidden = false;
+}
+
+function hide_pagination() {
+  ele.pagination_container.hidden = true;
+}
+
+function render_pagination() {
+  show_pagination();
+  clear_pagination();
+  get_page_amount();
+
+  var start
+    , end
+    , max_page_btn
+    , middle = Math.ceil(max_page_btn / 2)
+    , page_amount = share.get_page_amount()
+    , current_page = share.get_current_page()
+    , reaching_left = current_page <= middle
+    , reaching_left = current_page >= page_amount - middle
+  ;
+
+  if(reaching_left) {
+    start = 1;
+    end = max_page_btn;
+  } else if (reaching_right) {
+    start = page_amount - max_page_btn;
+    end = page_amount
+  } else {
+    start = current_page - middle + 1;
+    end = current_page + middle - 1;
+  }
+
+  if(start < 1) {
+    start = 1;
+  }
+
+  if(end > page_amount) {
+    end = page_amount;
+  }
+
+  for(var i = start; i <= end; i++) {
+    var num = i;
+    var btn = document.createElement('button');
+    btn.innerText = num;
+    btn.classList.add('pager');
+
+    if(current_page == num) {
+      btn.classList.add('active');
+    }
+
+    ele.pagination.appendChild(btn);
+    btn.addEventListener('click',make(num));
+  }
+
+  function make(num) {
+    return function() {
+      current_page = share.set_current_page(num);
+      search();
+    }
+  }
+}
+
+function clear_pagination() {
+  ele.pagination.innerHTML = '';
 }
 
 module.exports = {
