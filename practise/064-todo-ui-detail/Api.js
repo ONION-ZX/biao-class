f81ee4b6ff9a6a0329ccf3e110bf9dc55ed4eb5a
ONION-ZX <zxonion@gmail.com>
2018-05-19 23:42:36 +0800

    ...

---
+++ practise/064-todo-ui-detail/Api.js
@@ -0,0 +1,73 @@
+window.Api = Api;
+
+function Api(list, max_id) {
+  this.max_id = max_id || 1;
+  this.list = [
+    { // 0
+      id: 100,
+      title: '买菜',
+      remind_at: '2020...',
+      completed: false,
+    },
+    { // 1
+      id: 101,
+      title: '洗菜',
+      remind_at: '2020-10-01 20:20:02',
+      completed: false,
+    },
+  ];
+}
+
+Api.prototype.add = add;
+Api.prototype.remove = remove;
+Api.prototype.update = update;
+Api.prototype.read = read;
+
+/*增*/
+function add(row) {
+  this.max_id = this.max_id + 1;
+  row.id = this.max_id;
+  this.list.push(row);
+}
+
+function remove(id) {
+  var index = find_index_by_id(this.list, id);
+
+  if (index < 0)
+    return;
+
+  this.list.splice(index, 1);
+}
+
+function update(id, new_row) {
+  var index = find_index_by_id(this.list, id);
+
+  if (index < 0)
+    return;
+
+  delete new_row.id;
+
+  var old_row = this.list[index];
+  this.list[index] = Object.assign({}, old_row, new_row);
+}
+
+
+/*查*/
+function read(id) {
+  if (id)
+    return find_by_id(this.list, id);
+
+  return this.list;
+}
+
+function find_index_by_id(arr, id) {
+  return arr.findIndex(function (row) {
+    return row.id == id;
+  });
+}
+
+function find_by_id(arr, id) {
+  return arr.find(function (row) {
+    return row.id == id;
+  });
+}
--- practise/064-todo-ui-detail/Ui.js
+++ practise/064-todo-ui-detail/Ui.js
@@ -1,42 +1,108 @@
-function Ui(form_selector,list_selector) {
+window.Ui = Ui;
+
+function Ui(form_selector, list_selector, input_selector) {
   this.form = document.querySelector(form_selector);
+  this.input = document.querySelector(input_selector);
   this.list = document.querySelector(list_selector);
+  /*私有，不应该直接调用，仅限此文件内部调用*/
+  this._api = new Api();
 }
 
-Ui.prototype.set_form_data = set_form_data;
 Ui.prototype.get_form_data = get_form_data;
+Ui.prototype.set_form_data = set_form_data;
+Ui.prototype.render = render;
+Ui.prototype.init = init;
+Ui.prototype.detect_add = detect_add;
+Ui.prototype.detect_click_list = detect_click_list;
+Ui.prototype.remove = remove;
 
-function set_form_data(form_selector,data) {
-  var form = document.querySelector(form_selector);
-
-  for(var key in data) {
-    var value = data[key];
-    var input = form.querySelector(`name=${key}`);
-
-    if(!input)
-      continue;
-
-    var data_type = typeof value;
-
-    switch(data_type) {
-      case 'string':
-      case 'number':
-        input.value = value;
-        break;
-      case 'boolean':
-        input.checked = value;
-        break;
-    }
-  }
+function init() {
+  this.render();
+  this.detect_add();
+  this.detect_click_list();
 }
 
-function get_form_data(form_selector) {
+
+function detect_add() {
+  var me = this;
+  this.form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var row = me.get_form_data(me.form);
+    if (row.id) {
+      /*更新*/
+      me._api.update(row.id, row);
+    } else {
+      /*新增*/
+      me._api.add(row);
+    }
+    me.render();
+    me.input.value = '';
+  });
+}
+
+function detect_click_list() {
+  var me = this;
+  this.list.addEventListener('click', function (e) {
+    var target = e.target
+      , todo_item = target.closest('.todo-item')
+      , id = todo_item.dataset.id
+      , is_remove_btn = target.classList.contains('remove')
+      , is_update_btn = target.classList.contains('update')
+    ;
+
+    if (is_remove_btn) {
+      me.remove(id);
+    } else if (is_update_btn) {
+      var row = me._api.read(id);
+      me.set_form_data(me.form, row);
+    }
+  });
+}
+
+
+function remove(id) {
+  this._api.remove(id);
+  this.render();
+}
+
+function render() {
+  /*先通过api拿到所有数据*/
+  var todo_list = this._api.read();
+  var me = this;
+
+  /*清空上次渲染的数据*/
+  this.list.innerHTML = '';
+
+  todo_list.forEach(function (item) {
+    var el = document.createElement('div');
+
+    el.classList.add('row', 'todo-item');
+    el.dataset.id = item.id;
+
+    el.innerHTML = `
+      <div class="col checkbox">
+        <input type="checkbox">
+      </div>
+      <div class="col detail">
+        <div class="title">${item.title}</div>
+      </div>
+      <div class="col tool-set">
+        <button class="update">更新</button>
+        <button class="remove">删除</button>
+      </div>
+    `;
+
+    me.list.appendChild(el);
+  });
+}
+
+function get_form_data(form) {
   var data = {};
-  var form = document.querySelector(form_selector);
   var list = form.querySelectorAll('[name]');
 
-  list.forEach(function(input) {
+  list.forEach(function (input) {
     switch (input.nodeName) {
+
       case 'INPUT':
         switch (input.type) {
           case 'text':
@@ -52,12 +118,33 @@ function get_form_data(form_selector) {
             break;
         }
         break;
-        case 'TEXTAREA':
-          data[input.name] = input.value;
-          break;
+      case 'TEXTAREA':
+        data[input.name] = input.value;
+        break;
     }
   });
+
   return data;
 }
-var ui = new Ui('#todo-form','#todo-list');
-console.log(ui);
+
+function set_form_data(form, data) {
+  for (var key in data) {
+    var value = data[key];
+    var input = form.querySelector(`[name=${key}]`);
+
+    if (!input)
+      continue;
+
+    var data_type = typeof value;
+
+    switch (data_type) {
+      case 'string':
+      case 'number':
+        input.value = value;
+        break;
+      case 'boolean':
+        input.checked = value;
+        break;
+    }
+  }
+}
