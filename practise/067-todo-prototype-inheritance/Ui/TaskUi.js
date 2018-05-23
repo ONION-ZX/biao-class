ab28ecfdf965d489f3ed219c056dbfd5d905e824
ONION-ZX <zxonion@gmail.com>
2018-05-23 15:39:04 +0800

    TaskUi Finished

--- practise/067-todo-prototype-inheritance/Ui/TaskUi.js
+++ practise/067-todo-prototype-inheritance/Ui/TaskUi.js
@@ -1,24 +1,24 @@
-window.TaskUi = TaskUi;
+windor.TaskUi = TaskUi;
 test_list = [
-  { // 0
-    id        : 100,
-    title     : '买菜',
+  {
+    id : 001,
+    title : 'study',
     completed : false,
-    group_id  : 1,
-  },
-  { // 1
-    id        : 101,
-    title     : '洗菜',
-    completed : false,
-    group_id  : 1,
+    group_id : 1,
   },
   {
-    id        : 102,
-    title     : '背单词',
+    id : 002,
+    title : 'work',
     completed : false,
-    group_id  : 2,
+    group_id : 1,
   },
-];
+  {
+    id : 003,
+    title : 'entertainment',
+    completed : true,
+    group_id : 2,
+  },
+]
 
 function TaskUi(form_selector, list_selector, input_selector) {
   this.form = document.querySelector(form_selector);
@@ -29,65 +29,16 @@ function TaskUi(form_selector, list_selector, input_selector) {
 
 TaskUi.prototype.get_form_data = helper.get_form_data;
 TaskUi.prototype.set_form_data = helper.set_form_data;
-TaskUi.prototype.init = init;
 TaskUi.prototype.render = render;
+TaskUi.prototype.init = init;
 TaskUi.prototype.detect_add = detect_add;
 TaskUi.prototype.detect_click_list = detect_click_list;
 TaskUi.prototype.remove = remove;
-// TaskUi.prototype.clear_form = clear_form;
 
 function init() {
   this.render();
   this.detect_add();
   this.detect_click_list();
-}
-
-function remove(id) {
-  this._api.remove(id);
-  this.render();
-}
-
-function detect_click_list() {
-  var me = this;
-  this.list.addEventListener('click',function(e) {
-    var target = e.target
-      , todo_item = target.closest('.todo-item')
-      , is_remove_btn = target.classList.contains('remove')
-      , is_update_btn = target.classList.contains('update')
-      , id = todo_item.dataset.id;
-
-      if(is_remove_btn) {
-        me.remove(id);
-      } else if(is_update_btn) {
-        var row = me._api.read(id);
-        me.set_form_data(me.form, row);
-      }
-  });
-}
-
-function render() {
-  var todo_list = this._api.read();
-  var me = this;
-  this.list.innerHTML = '';
-
-  todo_list.forEach(function(item) {
-    var el = document.createElement('div');
-    el.classList.add('row','todo-item');
-    el.dataset.id = item.id;
-    el.innerHTML = `
-    <div class="col checkbox">
-      <input type="checkbox">
-    </div>
-    <div class="col detail">
-      <div class="title">${item.title}</div>
-    </div>
-    <div class="col tool-set">
-      <button class="update">更新</button>
-      <button class="remove">删除</button>
-    </div>
-    `;
-    me.list.appendChild(el);
-  });
 }
 
 function detect_add() {
@@ -96,12 +47,63 @@ function detect_add() {
     e.preventDefault();
     var row = me.get_form_data(me.form);
 
-    if(row.id) {
-      me._api.update(row.id,row);
-    } else {
+    if(row.id)
+      me._api.update(row.id, row);
+    else
       me._api.add(row);
-    }
+
     me.render();
     me.input.value = '';
+  });
+}
+
+function detect_click_list() {
+  var me = this;
+  this.list.addEventListener('click',function(e) {
+    var target = e.target
+      , todo_item = target.closest('.todo-item')
+      , id = todo_item.dataset
+      , is_remove_btn = target.classList.contains('remove')
+      , is_update_btn = target.classList.contains('update')
+    ;
+
+    if(is_remove_btn) {
+      me.remove(id);
+    } else if(is_update_btn) {
+      var row = me._api.read(id);
+      me.set_form_data(me.form, row);
+    }
+  });
+}
+
+function remove(id) {
+  this._api.remove(id);
+  this.render();
+}
+
+function render() {
+  var todo_list = this._api.read();
+  var me = this;
+
+  this.list.innerHTML = '';
+
+  todo_list.forEach(function(item) {
+    var el = document.createElement('div');
+    el.classList.add('row','todo-item');
+    el.dataset.id = item.id;
+
+    el.innerHTML = `
+      <div class = "col checkbox">
+        <input type = "checkbox">
+      <div>
+      <div class = "col detail">
+        <div class="title">${item.title}</div>
+      </div>
+      <div class = "col tool-set">
+        <button class="update">更新</button>
+        <button class="remove">删除</button>
+      </div>
+    `;
+    me.list.appendChild(el);
   });
 }
