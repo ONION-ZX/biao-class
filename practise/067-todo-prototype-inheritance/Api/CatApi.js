3f4ab21f3eea0edb0f4980195304ef7c4afefe62
ONION-ZX <zxonion@gmail.com>
2018-05-23 15:17:00 +0800

    CatApi Finished

--- practise/067-todo-prototype-inheritance/Api/CatApi.js
+++ practise/067-todo-prototype-inheritance/Api/CatApi.js
@@ -2,31 +2,29 @@ window.CatApi = CatApi;
 
 function CatApi(list, max_id) {
   this.config = {
-    title : {
-      max_length : 10,
+    title: {
+      max_length: 10,
     },
   };
 
   list = list || [
     {
-      id    : 1,
-      title : '默认',
+      id: 1,
+      title: 'default',
     },
     {
-      id    : 2,
-      title : '生活是我大爷',
+      id: 2,
+      title: 'livelihood',
     },
     {
-      id    : 3,
-      title : '天天向上',
+      id: 3,
+      title: 'work',
     },
   ];
   max_id = max_id || 1;
-  /*继承显性属性（也就是原型prototype）*/
   BaseApi.call(this, list, max_id);
 }
 
-/*继承隐性属性（也就是原型prototype）*/
 CatApi.prototype = Object.create(BaseApi.prototype);
 CatApi.prototype.constructor = CatApi;
 
@@ -35,23 +33,21 @@ CatApi.prototype.remove = remove;
 CatApi.prototype.update = update;
 CatApi.prototype.read = read;
 
-
 function add(row) {
-  if (!row.title)
+  if(!row.title)
     return;
-
   var max_length = this.config.title.max_length;
 
-  if (row.title.length > max_length)
+  if(row.title.max_length > max_length)
     throw `title should not greater than ${max_length}`;
 
   return this.$add(row);
 }
 
 function remove(id) {
-  if (id == 1)
+  //默认项不能被删除
+  if(id == 1)
     return;
-
   return this.$remove(id);
 }
 
