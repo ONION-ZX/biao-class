dff928f472f4505ee62ba9d3678849e860bade45
ONION-ZX <zxonion@gmail.com>
2018-05-25 00:57:50 +0800

    basic function implement

--- practise/067-todo-prototype-inheritance/Ui/CatUi.js
+++ practise/067-todo-prototype-inheritance/Ui/CatUi.js
@@ -17,6 +17,7 @@ CatUi.prototype.hide_add_cat_btn = hide_add_cat_btn;
 CatUi.prototype.detect_click_add_cat_btn = detect_click_add_cat_btn;
 CatUi.prototype.detect_form_submit = detect_form_submit;
 CatUi.prototype.detect_click_form = detect_click_form;
+CatUi.prototype.detect_click_list = detect_click_list;
 CatUi.prototype.get_form_data = helper.get_form_data;
 CatUi.prototype.set_form_data = helper.set_form_data;
 CatUi.prototype.clear_form = helper.clear_form;
@@ -26,6 +27,7 @@ function init() {
   this.detect_click_add_cat_btn();
   this.detect_form_submit();
   this.detect_click_form();
+  this.detect_click_list();
 }
 
 function render() {
@@ -86,6 +88,25 @@ function detect_click_form() {
       if(is_cancel_btn) {
         me.hide_cat_form();
         me.show_add_cat_btn();
+      }
+  });
+}
+
+function detect_click_list() {
+  var me = this;
+  this.list.addEventListener('click',function(e) {
+    var target = e.target
+      , cat_item = target.closest('.cat-item')
+      , is_delete_btn = target.classList.contains('delete')
+      , is_update_btn = target.classList.contains('update');
+
+    if(cat_item) {
+      var id = cat_item.dataset.id;
+    }
+
+      if(is_delete_btn) {
+        me._api.remove(id);
+        me.render();
       }
   });
 }
