5a7e3a7b63d0f897001067139b38a1e00e6567c1
ONION-ZX <zxonion@gmail.com>
2018-06-14 17:21:11 +0800

    memo add delete

--- practise/071-vue-memo/index.js
+++ practise/071-vue-memo/index.js
@@ -1,26 +1,47 @@
-window.root = new Vue({
+window.root =new Vue({
     el: '#root',
     data: {
-        current: {},
-        memo_list: [
-            {
-                title: 'title A',
-                content: 'aaaa',
-            },
-            {
-                title: 'title B',
-                content: 'bbbb',
-            },
-            {
-                title: 'title C',
-                content: 'cccc',
-            },
-        ],
+        current : null,
+        memo_list : [],
     },
     methods: {
         add() {
-            this.current = {},
+            this.current = {};
             this.memo_list.push(this.current);
-        }
+        },
+        remove(memo) {
+            let index = this.memo_list.indexOf(memo);
+            this.memo_list.splice(index, 1);
+        },
+        sync_to(memo_list) {
+            localStorage.setItem('memo_list',JSON.stringify(memo_list));
+        },
+        sync_form() {
+            this.memo_list = JSON.parse(localStorage.getItem('memo_list')) || [
+                {
+                    title: 'title A',
+                    content: 'AAAAAAAA',
+                },
+                {
+                    title: 'title B',
+                    content: 'BBBBBBBB',
+                },
+                {
+                    title: 'title C',
+                    content: 'CCCCCCC',
+                },
+            ];
+        },
+    },
+    watch: {
+        memo_list: {
+            deep: true,
+            handler: function(data) {
+                this.sync_to(data);
+            },
+        },
+    },
+    created() {
+        this.sync_form();
     },
 });
